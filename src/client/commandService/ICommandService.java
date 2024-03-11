74e350954ce077302e26eed0bd5a0a1e36961441
Victoria <114642606+snake-overlord@users.noreply.github.com>
2024-03-11 18:56:21 +0300

    Delete src/client/commandService/ICommandService.java

--- src/client/commandService/ICommandService.java
+++
@@ -1,9 +0,0 @@
-package commandService;
-
-import java.util.List;
-
-public interface ICommandService {
-    void addCommand(BaseCommand command);
-    void addCommands(List<BaseCommand> commands);
-    void executeCommand(String[] command);
-}
