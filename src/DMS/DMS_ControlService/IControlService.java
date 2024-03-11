65ced4ad21f10dca33b8c285fdde0fd44e58526e
Victoria <114642606+snake-overlord@users.noreply.github.com>
2024-03-11 19:05:22 +0300

    Add files via upload

---
+++ src/DMS/DMS_ControlService/IControlService.java
@@ -0,0 +1,14 @@
+package DMS.DMS_ControlService;
+
+import DMS.models.Organization;
+
+import java.io.FileNotFoundException;
+import java.util.Vector;
+
+public interface IControlService {
+    Vector<Organization> create();
+    Vector<Organization> read() throws FileNotFoundException;
+    void update(Vector<Organization> current);
+    void delete(Vector<Organization> org);
+
+}
